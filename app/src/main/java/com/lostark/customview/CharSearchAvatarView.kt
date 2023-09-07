36bc596bc41b81628ee635beb945fb273bcf7386
Geon <Geon@DESKTOP-JUE8HQB>
2023-09-07 14:13:16 +0900

    2023-09-07 14:11 raid detail TabLayout추가
    캐릭터 서치부분에 아바타 발자국에 심볼스트링이 없으면 오류나는거 해결
    ----
    초기에 API키 입력하는 부분 ui 구현해야함
    API키가 유효한지 확인하는것
    레이드 이미지들 추가해야함
    욕심나는거는 빙파고/재련최적화/각인검색기 정도

--- app/src/main/java/com/lostark/customview/CharSearchAvatarView.kt
+++ app/src/main/java/com/lostark/customview/CharSearchAvatarView.kt
@@ -19,21 +19,21 @@ import com.lostark.loahelper.SearchDetailActivity
 class CharSearchAvatarView : LinearLayout {
 
 
-    lateinit var leftLayout:LinearLayout
-    lateinit var rightLayout:LinearLayout
+    lateinit var leftLayout: LinearLayout
+    lateinit var rightLayout: LinearLayout
     lateinit var avatarImage: ImageView
     lateinit var avatarName: TextView
     lateinit var avatarType: TextView
     lateinit var avatarNameLeft: TextView
     lateinit var avatarTypeLeft: TextView
 
-    var imageUrl=""
-    var defaultEffect=""//기본효과
-    var tendenciesEffect=""//성향효과
-    lateinit var avatarNameString:String//아바타이름
-    lateinit var avatarTypeString:String//ex 무기 아바타
-    lateinit var avatarGrade:String//아바타 등급
-    lateinit var avatarLine:String//아바타 계열 ex 건슬링어 전용
+    var imageUrl = ""
+    var defaultEffect = ""//기본효과
+    var tendenciesEffect = ""//성향효과
+    lateinit var avatarNameString: String//아바타이름
+    lateinit var avatarTypeString: String//ex 무기 아바타
+    lateinit var avatarGrade: String//아바타 등급
+    lateinit var avatarLine: String//아바타 계열 ex 건슬링어 전용
 
     constructor(context: Context?) : super(context) {
         init(context)
@@ -58,24 +58,25 @@ class CharSearchAvatarView : LinearLayout {
         rightLayout = view.findViewById(R.id.char_search_detail_avatar_right_layout)
     }
 
-    private fun getAttrs(attrs: AttributeSet?){
-        val typedArray = context.obtainStyledAttributes(attrs,R.styleable.AvatarViewAttr)
+    private fun getAttrs(attrs: AttributeSet?) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.AvatarViewAttr)
         avatarType.text = typedArray.getText(R.styleable.AvatarViewAttr_avatarTypeText)
         avatarTypeLeft.text = typedArray.getText(R.styleable.AvatarViewAttr_avatarTypeText)
-        val avatarLeftRight = typedArray.getText(R.styleable.AvatarViewAttr_avatarLeftRight).toString()
-        if(avatarLeftRight== "right"){
+        val avatarLeftRight =
+            typedArray.getText(R.styleable.AvatarViewAttr_avatarLeftRight).toString()
+        if (avatarLeftRight == "right") {
             leftLayout.visibility = View.GONE
             rightLayout.visibility = View.VISIBLE
-        }else if(avatarLeftRight == "left"){
+        } else if (avatarLeftRight == "left") {
             leftLayout.visibility = View.VISIBLE
             rightLayout.visibility = View.GONE
         }
         typedArray.recycle()
     }
 
-    fun setImageBackground(grade : String){
-        avatarGrade=grade
-        when(grade) {//이미지 백그라운드
+    fun setImageBackground(grade: String) {
+        avatarGrade = grade
+        when (grade) {//이미지 백그라운드
             "고대" -> {
                 avatarImage.setBackgroundResource(R.drawable.ancient_background)
                 avatarName.setTextColor(Color.parseColor("#d9ae43"))
@@ -111,44 +112,56 @@ class CharSearchAvatarView : LinearLayout {
         }
     }
 
-    fun setImageText(avatar: ArmoryAvatar,tooltip:Tooltip) {
-        imageUrl=avatar.icon
+    fun setImageText(avatar: ArmoryAvatar, tooltip: Tooltip) {
+        imageUrl = avatar.icon
 
         Glide.with(this).load(avatar.icon).into(avatarImage)
 
         setImageBackground(avatar.grade)
 
-        if(avatar.type.contains("상의")||avatar.type.contains("하의")||avatar.type.contains("악기")||avatar.type.contains("얼굴1")){
-            rightLayout.visibility=View.GONE
-            leftLayout.visibility=View.VISIBLE
-            avatarNameLeft.visibility= View.VISIBLE
+        if (avatar.type.contains("상의") || avatar.type.contains("하의") || avatar.type.contains("악기") || avatar.type.contains(
+                "얼굴1"
+            )
+        ) {
+            rightLayout.visibility = View.GONE
+            leftLayout.visibility = View.VISIBLE
+            avatarNameLeft.visibility = View.VISIBLE
             avatarNameLeft.text = avatar.name
-        }
-        else{
-            rightLayout.visibility=View.VISIBLE
-            leftLayout.visibility=View.GONE
-            avatarName.visibility=View.VISIBLE
+        } else {
+            rightLayout.visibility = View.VISIBLE
+            leftLayout.visibility = View.GONE
+            avatarName.visibility = View.VISIBLE
             avatarName.text = avatar.name
         }
 
-        avatarNameString=avatar.name
+        avatarNameString = avatar.name
 
-        avatarTypeString=avatar.type
+        avatarTypeString = avatar.type
 
         avatarLine = tooltip.elements.get("Element_002")?.value.toString()
 
-        val itemPartKeyList = tooltip.elements.filter { it.value.type=="ItemPartBox"}.keys.toList()
-        if(itemPartKeyList.isNotEmpty()){
-            defaultEffect = (tooltip.elements.get(itemPartKeyList.get(0))?.value as ItemPartData).element1
+        val itemPartKeyList =
+            tooltip.elements.filter { it.value.type == "ItemPartBox" }.keys.toList()
+        if (itemPartKeyList.isNotEmpty()) {
+            defaultEffect =
+                (tooltip.elements.get(itemPartKeyList.get(0))?.value as ItemPartData).element1
         }
 
         val pattern = "지성 : \\d+|담력 : \\d+|매력 : \\d+|친절 : \\d+".toRegex()
-        val symbolString = (tooltip.elements.get(tooltip.elements.filter { it.value.type=="SymbolString"}.keys.toList()?.get(0))?.value as SymbolStringData)?.contentStr
+        var symbolString: String? = null
+        val symbolStringList =
+            tooltip.elements.filter { it.value.type == "SymbolString" }?.keys?.toList()
+        if (symbolStringList?.isNotEmpty() ?: false) {
+            symbolString =
+                (tooltip.elements.get(symbolStringList?.get(0))?.value as SymbolStringData).contentStr
+        }
+        //val symbolString = (tooltip.elements?.get(tooltip.elements.filter { it.value.type=="SymbolString"}?.keys?.toList()?.get(0))?.value as SymbolStringData)?.contentStr
         val tendenciesList = symbolString?.let {
-            pattern.findAll(it).forEach {
-                tendenciesEffect += it.value.replace(": ","+")+"\n"
+            pattern.findAll(it)?.forEach {
+                tendenciesEffect += it.value.replace(": ", "+") + "\n"
             }
         }
-        tendenciesEffect=tendenciesEffect.substring(0,tendenciesEffect.length-1)
+        if (tendenciesEffect.isNotEmpty())
+            tendenciesEffect = tendenciesEffect.substring(0, tendenciesEffect.length - 1)
     }
 }
--- app/src/main/java/com/lostark/loahelper/RaidDetailActivity.kt
+++ app/src/main/java/com/lostark/loahelper/RaidDetailActivity.kt
@@ -10,7 +10,10 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.ScrollView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
 import com.lostark.adapter.EventViewPagerAdapter
 import com.lostark.adapter.RaidViewPagerAdapter
 import com.lostark.customview.HomeButtonView
@@ -25,9 +28,13 @@ class RaidDetailActivity : AppCompatActivity() {
     }
 
     fun viewPagerSet(){
-        val raidImagePager = findViewById<ViewPager2>(R.id.raid_detail_viewPager)
+        //val raidImagePager = findViewById<ViewPager2>(R.id.raid_detail_viewPager)
         val raidName = intent.getStringExtra("RaidName")
+        val tabLayout = findViewById<TabLayout>(R.id.raid_detail_tab)
+        val scrollView = findViewById<ScrollView>(R.id.raid_detail_scroll_view)
         var raidDrawableList = mutableListOf<Int>()
+
+        val raidImage = findViewById<ImageView>(R.id.raid_detail_image)
         when (raidName){
             "valtan" -> {
                 raidDrawableList.add(R.drawable.valtan_1)
@@ -44,8 +51,23 @@ class RaidDetailActivity : AppCompatActivity() {
                 raidDrawableList.add(R.drawable.koukusaton_3)
             }
         }
-        val raidViewPagerAdapter = RaidViewPagerAdapter(raidDrawableList,raidImagePager)
-        raidImagePager.adapter = raidViewPagerAdapter
+        raidImage.setImageResource(raidDrawableList.get(0))
+
+        raidDrawableList.forEachIndexed { index, i ->
+            tabLayout.addTab(tabLayout.newTab().setText((index+1).toString()+"관문"))
+        }
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                raidImage.setImageResource(raidDrawableList.get(tab?.position!!))
+                scrollView.fullScroll(ScrollView.FOCUS_UP)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+        /*val raidViewPagerAdapter = RaidViewPagerAdapter(raidDrawableList,raidImagePager)
+        raidImagePager.adapter = raidViewPagerAdapter*/
     }
 
 }
